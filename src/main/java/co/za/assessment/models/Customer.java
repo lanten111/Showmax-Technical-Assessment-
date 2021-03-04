b6c0f1a0e6a571a61cc684831939211dd3152d65
makhado <makhadolucky@gmail.com>
2021-03-05 00:17:03 +0200

    created entity

--- src/main/java/co/za/assessment/models/Customer.java
+++ src/main/java/co/za/assessment/models/Customer.java
@@ -3,12 +3,29 @@ package co.za.assessment.models;
 created by mumakhado on 2021/03/04
 */
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
 public class Customer {
 
-    String name;
-    String surname;
-    String id;
-    String customerNumber;
-    boolean statusActive;
+    @Id
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "customerNumber")
+    private String customerNumber;
+
+    @Column(name = "statusActive")
+    private boolean statusActive;
 
 }
