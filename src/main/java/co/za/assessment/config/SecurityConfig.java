f2c0f3a76639f13b2b91b572110476f62a900d95
makhado <makhadolucky@gmail.com>
2021-03-05 14:41:17 +0200

    added security

--- src/main/java/co/za/assessment/config/SecurityConfig.java
+++ src/main/java/co/za/assessment/config/SecurityConfig.java
@@ -2,6 +2,33 @@ package co.za.assessment.config;
 /*
 created by mumakhado on 2021/03/04
 */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-public class SecurityConfig {
-}
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+        @Override
+        protected void configure(HttpSecurity http) throws Exception
+        {
+            http
+                    .csrf().disable()
+                    .authorizeRequests().anyRequest().authenticated()
+                    .and()
+                    .httpBasic();
+        }
+
+        @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder auth)
+                throws Exception
+        {
+            auth.inMemoryAuthentication()
+                    .withUser("technical")
+                    .password("{noop}Assessment")
+                    .roles("USER");
+        }
+    }
+
